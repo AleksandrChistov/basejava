e41f36b768160bbbb0f27552a8acc0e165d1f21d
Александр Чистов <aleksandrchistov.ru@yandex.ru>
2024-05-12 11:50:24 +0700

    implemented size method

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -59,6 +59,11 @@ public class ArrayStorage {
     }
 
     int size() {
-        return 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                return i;
+            }
+        }
+        return storage.length;
     }
 }
