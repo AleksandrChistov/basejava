9cc38a19af2f0bbb4e5c6fecba7f8ebd4fc1df54
Александр Чистов <aleksandrchistov.ru@yandex.ru>
2024-06-13 21:11:37 +0700

    moved Period inside Organization

--- src/ru/javawebinar/basejava/model/Organization.java
+++ src/ru/javawebinar/basejava/model/Organization.java
@@ -1,5 +1,9 @@
 package ru.javawebinar.basejava.model;
 
+import ru.javawebinar.basejava.utils.DateUtil;
+
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -7,6 +11,10 @@ public class Organization {
     private final String name;
     private final String website;
     private final List<Period> periods;
+
+    public Organization(String name, String website, Period... periods) {
+        this(name, website, Arrays.asList(periods));
+    }
 
     public Organization(String name, String website, List<Period> periods) {
         Objects.requireNonNull(name, "name must not be null");
@@ -52,5 +60,70 @@ public class Organization {
                 ", website='" + website + '\'' +
                 ", periods=" + periods +
                 '}';
+    }
+
+    public static class Period {
+        private final LocalDate startDate;
+        private final LocalDate endDate;
+        private final String title;
+        private final String description;
+
+        public Period(LocalDate startDate, String title, String description) {
+            this(startDate, DateUtil.NOW, title, description);
+        }
+
+        public Period(LocalDate startDate, LocalDate endDate, String title, String description) {
+            this.startDate = Objects.requireNonNull(startDate, "startDate must not be null");
+            this.endDate = Objects.requireNonNull(endDate, "endDate must not be null");
+            this.title = Objects.requireNonNull(title, "title must not be null");
+            this.description = description;
+        }
+
+        public LocalDate getStartDate() {
+            return startDate;
+        }
+
+        public LocalDate getEndDate() {
+            return endDate;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Period period = (Period) o;
+            return startDate.equals(period.startDate) &&
+                    endDate.equals(period.endDate) &&
+                    title.equals(period.title) &&
+                    Objects.equals(description, period.description);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = startDate.hashCode();
+            result = 31 * result + endDate.hashCode();
+            result = 31 * result + title.hashCode();
+            result = 31 * result + Objects.hashCode(description);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return "Period{" +
+                    "startDate=" + startDate +
+                    ", endDate=" + endDate +
+                    ", title='" + title + '\'' +
+                    ", description='" + description + '\'' +
+                    '}';
+        }
     }
 }
