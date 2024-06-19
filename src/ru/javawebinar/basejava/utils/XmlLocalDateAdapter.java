a846f09c5265216ea0f15a2fdccb587df369f340
Александр Чистов <aleksandrchistov.ru@yandex.ru>
2024-06-19 19:02:28 +0700

    removed redundant exceptions

--- src/ru/javawebinar/basejava/utils/XmlLocalDateAdapter.java
+++ src/ru/javawebinar/basejava/utils/XmlLocalDateAdapter.java
@@ -5,12 +5,12 @@ import java.time.LocalDate;
 
 public class XmlLocalDateAdapter extends XmlAdapter<String, LocalDate> {
     @Override
-    public LocalDate unmarshal(String str) throws Exception {
+    public LocalDate unmarshal(String str) {
         return LocalDate.parse(str);
     }
 
     @Override
-    public String marshal(LocalDate ld) throws Exception {
+    public String marshal(LocalDate ld) {
         return ld.toString();
     }
 }
